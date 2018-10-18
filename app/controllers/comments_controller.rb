13c43e9f7b617e64d9e7c4e313b8a3830856a4b8
LifeH <hwie.zhu@gmail.com>
2018-10-17 22:22:30 -0400

    fixed threads

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    p params
+    @comments = Comment.where(post_id: params[:post_id])
     render json: {comments: @comments}
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments, path: '/api/comments'
-  resources :posts, path: '/api/posts'
+
+  resources :posts, path: '/api/posts' do 
+    resources :comments
+  end 
   resources :users, path: '/api/users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

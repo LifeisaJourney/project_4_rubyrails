46f3486bb2010331ebed49846383f9ebe5f7a4c8
LifeH <hwie.zhu@gmail.com>
2018-10-18 13:49:21 -0400

    bundle install

--- Gemfile
+++ Gemfile
@@ -26,8 +26,12 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
+gem 'bcrypt', '~> 3.1.7'
+gem 'active_model_serializers'
+gem 'rack-cors'
+gem 'knock'
+gem 'jwt'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
     resources :comments
   end 
   resources :users, path: '/api/users'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+#   get ‘/signup’ => ‘users#new’
+# post ‘/users’ => ‘users#create’
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

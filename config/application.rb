fb1687aba923ed45e226d41a7568e5d6f7c75de4
LifeH <hwie.zhu@gmail.com>
2018-10-18 23:09:30 -0400

    fixed cors

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Testapp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
       end
     end
   end

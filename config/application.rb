0b94dac563a9adb21c56aa3cb3f3dc1b8b7087e6
LifeH <hwie.zhu@gmail.com>
2018-10-20 23:21:05 -0400

    added patch and put to application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Testapp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :options, :delete]
       end
     end
   end
